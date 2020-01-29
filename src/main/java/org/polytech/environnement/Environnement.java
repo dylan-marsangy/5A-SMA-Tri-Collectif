b4bd677913ae6d1bb8f7ac8b88f6a8ff5f7a6e57
Dylan <dylanmarsangy@outlook.fr>
2020-01-29 15:42:03 +0100

    perception fonctionnelle avec tests

--- src/main/java/org/polytech/environnement/Environnement.java
+++ src/main/java/org/polytech/environnement/Environnement.java
@@ -1,6 +1,12 @@
 package org.polytech.environnement;
 
 import javafx.util.Pair;
+import org.polytech.agent.Agent;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Environnement {
 
@@ -25,6 +31,37 @@ public class Environnement {
         remove(x, y);
     }
 
+    /**
+     * Perception d'une entité dans toutes les directions sur une certaine distance.
+     * @param entity Entité qui cherche à scanner son environnement
+     * @param d Distance à scanner
+     * @return Objets qui se trouve à la d-ième case dans toutes les directions par rapport à l'entité
+     */
+    public Map<Direction, Movable> perception(Movable entity, int d) {
+        Map<Direction, Movable> neighbors = new HashMap<>();
+
+        for (Direction direction : Direction.values()) {
+            neighbors.put(direction, perception(entity, direction, d));
+        }
+
+        return neighbors;
+    }
+
+    /**
+     * Perception d'une entité dans une direction sur une certaine distance.
+     * @param entity Entité qui cherche à scanner son environnement
+     * @param direction Direction dans laquelle l'entité scanne
+     * @param d Distance à scanner
+     * @return Objet qui se trouve à la d-ième case par rapport à l'entité dans la direction souhaitée
+     */
+    public Movable perception(Movable entity, Direction direction, int d) {
+        Pair<Integer, Integer> coordinates = findEntity(entity);
+        int xGoal = coordinates.getKey() + d * direction.x;
+        int yGoal = coordinates.getValue() + d * direction.y;
+
+        return getEntity(xGoal, yGoal);
+    }
+
     public void remove(int x, int y) {
         if (isInside(x, y)) this.grid[x][y] = null;
         else throw new CollisionException("Vous êtes en train de casser les murs. >:(");
@@ -39,7 +76,7 @@ public class Environnement {
     }
 
     public Movable getEntity(int x, int y) {
-        return this.grid[x][y];
+        return (isInside(x, y)) ? this.grid[x][y] : null;
     }
 
     public boolean isInside(int x, int y) {
--- src/test/java/org/polytech/environnement/EnvironnementTest.java
+++ src/test/java/org/polytech/environnement/EnvironnementTest.java
@@ -1,15 +1,23 @@
 package org.polytech.environnement;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.polytech.agent.Agent;
+
+import java.util.AbstractMap;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Environnement Tests")
 public class EnvironnementTest {
 
-    private static final int n = 3;
-    private static final int m = 3;
+    private static final int n = 5;
+    private static final int m = 5;
     private static final int t = 10;
 
     private Environnement environnement;
@@ -83,6 +91,7 @@ public class EnvironnementTest {
         assertNull(environnement.getEntity(1, 1));
         assertEquals(agent, environnement.getEntity(0, 1));
     }
+
     @Test
     @DisplayName("Avoid Moves Outside Grid")
     public void avoidMoving_outsideGrid() {
@@ -99,6 +108,47 @@ public class EnvironnementTest {
         environnement.insert(a1, 0, 1);
         environnement.insert(a2, 1, 1);
         assertThrows(CollisionException.class, () -> environnement.move(a2, Direction.NORTH));
+    }
+
+    // PERCEPTION ------------------------------------------------------------------------------------------------------
+
+    @Test
+    @DisplayName("Get Entity Perception For Direction")
+    public void getPerception_oneDirection() {
+        Agent agent = new Agent(t);
+        Agent a2 = new Agent(t);
+        Agent a3 = new Agent(t);
+
+        environnement.insert(agent, 2, 2);
+        environnement.insert(a2, 1, 2);
+        environnement.insert(a3, 0, 2);
+
+        assertEquals(a2, environnement.perception(agent, Direction.NORTH, 1));
+        assertEquals(a3, environnement.perception(agent, Direction.NORTH, 2));
+    }
+
+    @Test
+    @DisplayName("Get Entity Perception For All Directions")
+    public void getPerception_allDirections() {
+        Agent agent = new Agent(t);
+        Agent a2 = new Agent(t);
+        Agent a3 = new Agent(t);
+        Agent a4 = new Agent(t);
+        Agent a5 = new Agent(t);
+
+        environnement.insert(agent, 2, 2);
+        environnement.insert(a2, 0, 2);
+        environnement.insert(a3, 4, 2);
+        environnement.insert(a4, 2, 4);
+        environnement.insert(a5, 3, 3);
+
+        Map<Direction, Movable> expected = new HashMap<>();
+        expected.put(Direction.NORTH, a2);
+        expected.put(Direction.SOUTH, a3);
+        expected.put(Direction.EAST, a4);
+        expected.put(Direction.WEST, null);
+
+        assertEquals(expected, environnement.perception(agent, 2));
     }
 
 }
