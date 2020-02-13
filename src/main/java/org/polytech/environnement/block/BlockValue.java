261e9ad85de51863e839e9b9d5adc75352a91498
Dylan <dylanmarsangy@outlook.fr>
2020-02-13 14:40:04 +0100

    :recycle: Affichage console Blocks

--- src/main/java/org/polytech/environnement/block/BlockValue.java
+++ src/main/java/org/polytech/environnement/block/BlockValue.java
@@ -1,19 +1,29 @@
 package org.polytech.environnement.block;
 
+import org.polytech.utils.Color;
+
 /**
  * Valeur d'un bloc.
  */
 public enum BlockValue {
-    A("A"), B("B"), ZERO("0");
+    A("A", Color.BLUE_BACKGROUND), B("B", Color.RED_BACKGROUND), ZERO("0", Color.RESET);
 
+    /**
+     * Couleur d'affichage console de la valeur.
+     */
+    private Color color;
+    /**
+     * Valeur d'un bloc.
+     */
     private String value;
 
-    BlockValue(String value) {
+    BlockValue(String value, Color color) {
         this.value = value;
+        this.color = color;
     }
 
     @Override
     public String toString() {
-        return this.value;
+        return this.color + this.value + Color.RESET;
     }
 }
