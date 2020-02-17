f0617742ac119ac53e26b01b131c5d9eae6bec0f
Dylan <dylanmarsangy@outlook.fr>
2020-02-17 03:57:44 +0100

    Correction constante

--- src/main/java/org/polytech/SMAConstants.java
+++ src/main/java/org/polytech/SMAConstants.java
@@ -10,7 +10,7 @@ public class SMAConstants {
     /**
      * Nombre d'itérations des actions des agents (nombre de fois total où les agents agissent).
      */
-    public final static int ITERATION_LOOPS = 57000;
+    public final static int ITERATION_LOOPS = 1600000;
     /***
      * Fréquence à laquelle afficher la grille pendant l'exécution de l'application par rapport au nombre d'itérations des actions des agents.
      * Si la fréquence est à 0, alors seulement l'état initial et final de l'environnement sont montrés.
